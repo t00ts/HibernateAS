b7f9d86397b4b852b745734380238f821002363c
Juanjo Sanchez <juanchowman@gmail.com>
2013-01-17 14:16:03 +0100

    classpath

--- src/com/as/hibernate/TestCtrl.java
+++ src/com/as/hibernate/TestCtrl.java
@@ -5,7 +5,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+import com.as.controllers.CtrlHotel;
 import com.as.controllers.CtrlViatge;
+import com.as.data.Ciutat;
+import com.as.data.Client;
+import com.as.data.Data;
+import com.as.data.Habitacio;
+import com.as.data.Hotel;
+import com.as.data.HotelLowCost;
+import com.as.data.HotelSuperior;
 import com.as.data.Viatge;
 
 public class TestCtrl {
@@ -17,16 +28,37 @@ public class TestCtrl {
 	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
 		//comprobar ctrlviatge
+		Configuration cfg = new Configuration();
+		cfg.addAnnotatedClass(Ciutat.class);
+		cfg.addAnnotatedClass(Hotel.class);
+		cfg.addAnnotatedClass(HotelSuperior.class);
+		cfg.addAnnotatedClass(HotelLowCost.class);
+		cfg.addAnnotatedClass(Habitacio.class);
+		cfg.addAnnotatedClass(Client.class);
+		cfg.addAnnotatedClass(Viatge.class);
+		cfg.addAnnotatedClass(Data.class);
+
+		// Hibernate database configuration
+		cfg.configure ("hibernate.cfg.xml");
+
+		// Create tables
+		//new SchemaExport (cfg).create (true, true);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		CtrlViatge cv = new CtrlViatge();
+		/*CtrlViatge cv = new CtrlViatge(cfg);
 		Date dini=sdf.parse("09/01/2013");
 		Date dini2=sdf.parse("05/01/2013");
 		Date dini3=sdf.parse("01/01/2013");
 		Viatge v;
 		List<Viatge> lv;
-		lv=cv.getAll();
-		System.out.println ("DniClient: "+lv.get(0).getDniClient()+" Dataini: "+lv.get(0).getdataInici());
-		System.out.println ("DniClient: "+lv.get(1).getDniClient()+" Dataini: "+lv.get(1).getdataInici());
+		v=cv.get("436453K", dini);
+		System.out.println ("DniClient: "+v.getDniClient()+" Dataini: "+v.getdataInici());
+		System.out.println ("DniClient: "+v.getDniClient()+" Dataini: "+v.getdataInici());*/
+		CtrlHotel ch = new CtrlHotel(cfg);
+	
+		Hotel h;
+		List<Hotel> lh;
+		h=ch.get("Hotel NH", "Barcelona");
+		System.out.println ("NomHot: "+h.getNom()+" Ciutat: "+h.getNomCiutat()+" Preu: "+h.getPreu());
 	}
 
 }
