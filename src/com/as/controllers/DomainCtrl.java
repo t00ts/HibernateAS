98444a6c4e78e5b65f8b16263b67a0b2166bb3e4
Ivan Sanchez <ivansm90@gmail.com>
2013-01-18 06:23:46 +0100

    ni idea

--- src/com/as/controllers/DomainCtrl.java
+++ src/com/as/controllers/DomainCtrl.java
@@ -14,7 +14,7 @@ import com.as.data.Viatge;
 import com.as.data.primarykeys.ViatgePrimaryKey;
 import com.as.data.tuples.TupleCiutat;
 
-//import src.PagamentClient;
+import src.PagamentClient;
 
 public class DomainCtrl {
 
@@ -62,11 +62,12 @@ public class DomainCtrl {
 	}
 	
 	/**PagamentClient es una clase del cliente que tiene el stub (adaptador) para conectar al Sv **/
-	/*public boolean pagament(Integer numTarg, Date dataCad) {
-		boolean autoritzat = PagamentClient.pagament(numTarg, dataCad);
-		if (autoritzat) guardarCambios();
+	public String pagament(Integer numTarg, Date dataCad) {
+		System.out.println("XXXXXXXXXXX" + dataCad);
+		String autoritzat = PagamentClient.pagament(numTarg, dataCad);
+		if (autoritzat == "Autoritzat") guardarCambios();
 		return autoritzat;
-	}*/
+	}
 	
 	public List<TupleCiutat>  obteCiutats() {//devuelve una lista de nomciutat, preuvol de las ciudades del sistema
 		
