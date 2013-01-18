44522b2ed615fef8dd9e77d74923080476a59693
Ivan Sanchez <ivansm90@gmail.com>
2013-01-18 02:02:08 +0100

    DatePicker

--- src/com/as/views/FinestraSeleccioViatge.java
+++ src/com/as/views/FinestraSeleccioViatge.java
@@ -128,12 +128,12 @@ public class FinestraSeleccioViatge extends JFrame {
 		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblNewLabel_2.setBounds(219, 119, 83, 14);
 		contentPane.add(lblNewLabel_2);
-			
+
 		dI = new JXDatePicker();
 		Calendar calendar = dI.getMonthView().getCalendar();
 		calendar.setTime(new Date());
 		dI.getMonthView().setLowerBound(calendar.getTime());
-		dI.setFormats("dd/MM/yyy");
+		dI.setFormats("dd/MM/yyyy");
 		dI.setEditable(true);
 		dI.setVisible(true);
 		dI.setBounds(312, 116, 142, 20);
@@ -141,11 +141,14 @@ public class FinestraSeleccioViatge extends JFrame {
 		
 		//Sel.lecció de la data de tornada del viatje		
 		
+		
 		JLabel lblNewLabel_3 = new JLabel("Data Tornada:");
 		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblNewLabel_3.setBounds(219, 150, 83, 14);
 		contentPane.add(lblNewLabel_3);
 		dF = new JXDatePicker();
+		dF.getMonthView().setLowerBound(calendar.getTime());
+		dF.setFormats("dd/MM/yyyy");
 		dF.setEditable(true);
 		dF.setVisible(true);
 		dF.setBounds(312, 147, 142, 20);
